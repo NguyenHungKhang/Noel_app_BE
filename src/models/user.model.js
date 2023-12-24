68374b10424940922c4f8962d983c4c39846784e
NguyenHungKhang <104726874+NguyenHungKhang@users.noreply.github.com>
2023-12-24 20:47:32 +0700

    2

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,7 +5,6 @@ const userSchema = mongoose.Schema({
     _id: mongoose.Types.ObjectId,
     userCode : {type: String, trim: true, require: true, index: {unique: true}},
     ipv4: {type: String, trim: true, require: true, index: { unique: true }},
-    blog: {type: mongoose.Schema.Types.ObjectId, index: {unique: true}, ref: "Blog"},
 },  { timestamps: true, versionKey: false });
 
 const userModel = mongoose.model("User", userSchema);
